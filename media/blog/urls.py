14df98c9345da136d59c7c6b27a6c3528084e9f5
MrMeow2023 <154748738+MrMeow2023@users.noreply.github.com>
2024-01-21 21:51:50 +0800

    Delete media directory

--- media/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views #import from current folder the views.py
-
-urlpatterns = [
-    path("", views.index, name='index'),
-    path("events", views.start, name='posts-page'),
-    path("events/<slug:slug>", views.blog_post, name="post-detail-page"),
-    path("create_post", views.new_post, name="new-post-page")
-]
